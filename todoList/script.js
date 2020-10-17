24d841ce16ac0b5686ec169f73707a13f116b1c7
Nari87 <nari.moennig@gmail.com>
2020-10-17 12:37:39 +0200

    test

---
+++ todoList/script.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    /*
+        1. Add-Button is disabled when input Field is empty
+        2. Adds the text from the input field to the list when the "Add Task"-btn is clicked
+        3. Removes a selected List-item (li)
+    */
+    function addAndRemove(){
+        if(document.getElementById('inputTask').value == ""){
+            document.getElementById('addTask').disable = true;
+        } else {
+            let ul = document.getElementById('taskList');
+            let li = document.createElement('li');
+            let task = document.getElementById('inputTask').value;
+            li.appendChild(document.createTextNode(task));
+            ul.appendChild(li);
+            document.getElementById('inputTask').value = "";
+            li.onclick = function(){
+                this.parentNode.removeChild(this);
+            }
+        }
+    }
+   
+    //When the Add-Task Button is clicked the addAndRemove-function is called
+    document.getElementById('addTask').addEventListener("click", addAndRemove);
+
+    //The Enter Button gets the same function as the Add Task Button
+    document.getElementById('inputTask').addEventListener("keypress", function(event){
+        if(event.key == "Enter"){
+            addAndRemove();
+        }
+    });
+
+    //Fades out the infoBox when the x-symbol is clicked
+    document.getElementById('close').addEventListener("click", function(){
+        let fadeTarget = document.getElementById('info');
+        let fadeEffect = setInterval(function () {
+            if (!fadeTarget.style.opacity) {
+                fadeTarget.style.opacity = 0.5;
+            }
+            if (fadeTarget.style.opacity > 0) {
+                fadeTarget.style.opacity -= 0.1;
+            } else {
+                clearInterval(fadeEffect);
+            }
+        }, 50);
+        
+    });
+  
+
+
+
+});
